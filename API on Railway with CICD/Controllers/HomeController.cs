20348a6478ed063e7b9b198d53c00fb7f4a37aa1
kyrillosaziz <kyrillos.aziz@webenia.com>
2025-07-24 16:03:43 +0300

    add HomeController

---
+++ API on Railway with CICD/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_on_Railway_with_CICD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
